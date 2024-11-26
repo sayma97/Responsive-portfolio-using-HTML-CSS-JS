192ac5d6b8f21534a7568b0b52d8fe1be790abed
sayma97 <99041202+sayma97@users.noreply.github.com>
2024-11-27 00:19:02 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//---------- Navbar ----------//
+
+window.addEventListener('scroll', function(){
+    let navbar = document.getElementById("navbar");
+    navbar.classList.toggle('fixed', this.window.scrollY > 0)
+})
+
+// Portfolio Gallery
+
+let filterContainer = document.querySelector(".gallery-filter");
+let galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")){
+        // deactivates existing active filter item
+        filterContainer.querySelector(".active").classList.remove("active");
+        // activates new filter item
+        event.target.classList.add("active");
+        let filterValue = event.target.getAttribute("data-filter");
+        galleryItems.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === "all") {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+
+});
